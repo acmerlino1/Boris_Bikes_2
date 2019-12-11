f63413f15ccdc59754155672019ebfa880d66f20
shadz22 <shadz22@yahoo.co.uk>
2019-12-11 17:56:16 +0000

    raise an error when docking station is full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,13 +11,19 @@ describe DockingStation do
 
     it 'docks a bike' do
         bike = Bike.new
-      expect(docking_station.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq bike
     end
 
-    it 'returns docked bikes' do
-        bike = Bike.new
-        docking_station.dock(bike)
-        expect(docking_station.bike).to eq bike
+    # it 'returns docked bikes' do
+    #     bike = Bike.new
+    #     docking_station.dock(bike)
+    #     expect(docking_station.bike).to eq bike
+    #end
+
+    it 'counts docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
     end
 
     it { is_expected.to respond_to(:dock).with(1).argument }
@@ -27,11 +33,18 @@ describe DockingStation do
     describe '#release_bike' do
         it 'raises an error when there are no bikes available' do
             expect { subject.release_bike }.to raise_error 'No bikes available'
-        end 
+        end
         it 'releases a bike' do
             bike = Bike.new
             subject.dock(bike)
             expect(subject.release_bike).to eq bike
         end
+    end
+
+    describe '#dock' do
+      it 'raises an error when docking station is full' do
+        subject.dock(Bike.new)
+        expect {subject.dock(Bike.new)}.to raise_error 'Docking station full'
+      end
     end
 end
