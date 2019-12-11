4a464c4b56282d9e427ba1044ac25c82e07e9183
AliciaMerlino <acmerlino1@gmail.com>
2019-12-11 14:46:35 +0000

    added test to DockingStation, check if bike is working, and release new bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
+    docking_station = DockingStation.new
     it { is_expected.to respond_to :release_bike }
+
+    it 'releases bike' do
+    bike = docking_station.release_bike
+    expect(bike).to be_working
+    end
 end
