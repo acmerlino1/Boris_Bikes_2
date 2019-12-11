3eb3243bcb5a6e51e4108403e248c04415166122
Asia <asiaellis5@gmail.com>
2019-12-11 15:53:54 +0000

    added method to make docking possible

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,22 @@
 require_relative 'bike'
 
 class DockingStation
+    # attr_reader :bike
+    
     def release_bike
       Bike.new
     end
+
+    def dock(bike)
+      @bike = bike
+    end
+
+    def bike
+       @bike
+    end
+
+    # def dock(bike)
+    #   @bike = bike
+    # end
+
 end
