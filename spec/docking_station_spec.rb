afaac6803a0fc8876bda2daf9c3f50704cdb7ee9
AliciaMerlino <acmerlino1@gmail.com>
2019-12-11 16:53:50 +0000

    Bikes cannot be released if there are no bikes at docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,7 @@ describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 
     it 'releases working bike' do
-    bike = docking_station.release_bike
+    bike = Bike.new
     expect(bike).to be_working
     end
 
@@ -23,4 +23,15 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it { is_expected.to respond_to(:bike) }
+
+    describe '#release_bike' do
+        it 'raises an error when there are no bikes available' do
+            expect { subject.release_bike }.to raise_error 'No bikes available'
+        end 
+        it 'releases a bike' do
+            bike = Bike.new
+            subject.dock(bike)
+            expect(subject.release_bike).to eq bike
+        end
+    end
 end
